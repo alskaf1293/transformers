fa259507dab92cfc5d1c6cb3a3297b4a033719be
Timothy Oh <timothyohoc@gmail.com>
2022-12-31 16:35:39 -0800

    made changes

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-"""
-Seq2Seq using Transformers on the Multi30k
-dataset. In this video I utilize Pytorch
-inbuilt Transformer modules, and have a
-separate implementation for Transformers
-from scratch. Training this model for a
-while (not too long) gives a BLEU score
-of ~35, and I think training for longer
-would give even better results.
-"""
-
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -49,90 +38,6 @@ train_data, valid_data, test_data = Multi30k.splits(
 german.build_vocab(train_data, max_size=10000, min_freq=2)
 english.build_vocab(train_data, max_size=10000, min_freq=2)
 
-"""
-class Transformer(nn.Module):
-    def __init__(
-        self,
-        embedding_size,
-        src_vocab_size,
-        trg_vocab_size,
-        src_pad_idx,
-        num_heads,
-        num_encoder_layers,
-        num_decoder_layers,
-        forward_expansion,
-        dropout,
-        max_len,
-        device,
-    ):
-        super(Transformer, self).__init__()
-        self.src_word_embedding = nn.Embedding(src_vocab_size, embedding_size)
-        self.src_position_embedding = nn.Embedding(max_len, embedding_size)
-        self.trg_word_embedding = nn.Embedding(trg_vocab_size, embedding_size)
-        self.trg_position_embedding = nn.Embedding(max_len, embedding_size)
-
-        self.device = device
-        
-        self.transformer = nn.Transformer(
-            embedding_size,
-            num_heads,
-            num_encoder_layers,
-            num_decoder_layers,
-            forward_expansion,
-            dropout,
-        )
-        self.fc_out = nn.Linear(embedding_size, trg_vocab_size)
-        
-        self.transformer = nn.Transformer()
-        self.dropout = nn.Dropout(dropout)
-        self.src_pad_idx = src_pad_idx
-
-    def make_src_mask(self, src):
-        src_mask = src.transpose(0, 1) == self.src_pad_idx
-
-        # (N, src_len)
-        return src_mask.to(self.device)
-
-    def forward(self, src, trg):
-        src_seq_length, N = src.shape
-        trg_seq_length, N = trg.shape
-
-        src_positions = (
-            torch.arange(0, src_seq_length)
-            .unsqueeze(1)
-            .expand(src_seq_length, N)
-            .to(self.device)
-        )
-
-        trg_positions = (
-            torch.arange(0, trg_seq_length)
-            .unsqueeze(1)
-            .expand(trg_seq_length, N)
-            .to(self.device)
-        )
-
-        embed_src = self.dropout(
-            (self.src_word_embedding(src) + self.src_position_embedding(src_positions))
-        )
-        embed_trg = self.dropout(
-            (self.trg_word_embedding(trg) + self.trg_position_embedding(trg_positions))
-        )
-
-        src_padding_mask = self.make_src_mask(src)
-        trg_mask = self.transformer.generate_square_subsequent_mask(trg_seq_length).to(
-            self.device
-        )
-
-        out = self.transformer(
-            embed_src,
-            embed_trg,
-            src_key_padding_mask=src_padding_mask,
-            tgt_mask=trg_mask,
-        )
-        out = self.fc_out(out)
-        return out
-"""
-
 # We're ready to define everything we need for training our Seq2Seq model
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 #print(device)
@@ -170,21 +75,7 @@ train_iterator, valid_iterator, test_iterator = BucketIterator.splits(
     sort_key=lambda x: len(x.src),
     device=device,
 )
-"""
-model = Transformer(
-    embedding_size,
-    src_vocab_size,
-    trg_vocab_size,
-    src_pad_idx,
-    num_heads,
-    num_encoder_layers,
-    num_decoder_layers,
-    forward_expansion,
-    dropout,
-    max_len,
-    device,
-).to(device)
-"""
+
 model = Transformer(srcVocabSize, trgVocabSize, pad_idx, 0).to(device)
 
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
